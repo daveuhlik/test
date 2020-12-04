8537d055e4cf2ebbd730df221cdf99bab5fdb358
Dave Uhlik <49681728+daveuhlik@users.noreply.github.com>
2020-12-04 11:58:04 +0100

    Add files via upload
    Upload files

---
+++ fes.js
@@ -0,0 +1,122 @@
+
+const anitaImage = document.getElementById('anita');
+const janosImage = document.getElementById('janos');
+const mariannaImage = document.getElementById('marianna');
+const jorgImage = document.getElementById('jorg');
+const csillaImage = document.getElementById('csilla');
+const beataImage = document.getElementById('beata');
+const zsoltImage = document.getElementById('zsolt');
+const editImage = document.getElementById('edit');
+const benceImage = document.getElementById('bence');
+const bzsoltImage = document.getElementById('bzsolt');
+
+
+$(function(){ 
+   anitaImage.onclick = function() {
+        const mySrc = anitaImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2634&sharep=y8bd8z3enc04w5f4ghf8foyuu') {
+            anitaImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2615&sharep=36vv3ibxzbxnj60g6a5m5ggiz');
+         } else {
+            anitaImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2634&sharep=y8bd8z3enc04w5f4ghf8foyuu');
+         }
+   };
+});
+
+$(function(){ 
+   janosImage.onclick = function() {
+        const mySrc = janosImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2635&sharep=n7ao5ziu5od2jhoheq762ekny') {
+            janosImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2626&sharep=u3t7xrjjzsemi0y74j2ypi3th');
+         } else {
+            janosImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2635&sharep=n7ao5ziu5od2jhoheq762ekny');
+         }
+   };
+});
+
+$(function(){ 
+   mariannaImage.onclick = function() {
+        const mySrc = mariannaImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2636&sharep=d0nk6rm2ezad5aeyiua73pfuk') {
+            mariannaImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2616&sharep=y4cmdphtbtzyk7czoa5f44zrb');
+         } else {
+            mariannaImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2636&sharep=d0nk6rm2ezad5aeyiua73pfuk');
+         }
+   };
+});
+
+$(function(){ 
+   jorgImage.onclick = function() {
+        const mySrc = jorgImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2641&sharep=zifmcydkj04aijfe5tyom7bef') {
+            jorgImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2622&sharep=7q6zqp3b40yf23mmsvoaufxyb');
+         } else {
+            jorgImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2641&sharep=zifmcydkj04aijfe5tyom7bef');
+         }
+   };
+});
+
+$(function(){ 
+   csillaImage.onclick = function() {
+        const mySrc = csillaImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2613&sharep=cooege8iw2ndcbn7icg0vimd6') {
+            csillaImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2612&sharep=5bjzyu5kytbmp4kqrem2icmes');
+         } else {
+            csillaImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2613&sharep=cooege8iw2ndcbn7icg0vimd6');
+         }
+   };
+});
+
+$(function(){ 
+   beataImage.onclick = function() {
+        const mySrc = beataImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2642&sharep=vqzt4i7vje0veua646jt4yocv') {
+            beataImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2625&sharep=jw2giuy2ratugt87hkpwuq4ws');
+         } else {
+            beataImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2642&sharep=vqzt4i7vje0veua646jt4yocv');
+         }
+   };
+});
+
+$(function(){ 
+   zsoltImage.onclick = function() {
+        const mySrc = zsoltImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2637&sharep=ivmu4qzfa0thkzir5dtvumou3') {
+            zsoltImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2614&sharep=fijgf5bd8kmndcqycucnnpjfs');
+         } else {
+            zsoltImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2637&sharep=ivmu4qzfa0thkzir5dtvumou3');
+         }
+   };
+});
+
+$(function(){ 
+   editImage.onclick = function() {
+        const mySrc = editImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2638&sharep=wdo4na2237xkve5q6bofbw74o') {
+            editImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2624&sharep=8pxn7bn76n4nra82n4ngdee3t');
+         } else {
+            editImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2638&sharep=wdo4na2237xkve5q6bofbw74o');
+         }
+   };
+});
+
+$(function(){ 
+   benceImage.onclick = function() {
+        const mySrc = benceImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2639&sharep=0yvgzrtjuvb6txccrd6xkui25') {
+            benceImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2621&sharep=xes5a4q3amaz2twib3xnmd0ph');
+         } else {
+            benceImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2639&sharep=0yvgzrtjuvb6txccrd6xkui25');
+         }
+   };
+});
+
+$(function(){ 
+   bzsoltImage.onclick = function() {
+        const mySrc = bzsoltImage.getAttribute('src');
+        if(mySrc === 'http://www.wellcom.hu/ftp/index.php?shareid=2640&sharep=20z282adm5wpnadsne2f4fqzq') {
+            bzsoltImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2623&sharep=wsp03wh2y36tstebxdmpv7foa');
+         } else {
+            bzsoltImage.setAttribute ('src','http://www.wellcom.hu/ftp/index.php?shareid=2640&sharep=20z282adm5wpnadsne2f4fqzq');
+         }
+   };
+});
